f803165b5fbcea231769cb8e6384e0bf179ad2f4
ciliamadani <40335630+ciliamadani@users.noreply.github.com>
2019-12-22 22:48:17 +0100

    Add files via upload

---
+++ ExtractionDeLinformation.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[19]:
+
+
+from bs4 import BeautifulSoup
+import requests
+import re
+
+
+# In[20]:
+
+
+lettres=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+bonInterval = False
+
+#Recuperer l'intervalle 
+while(bonInterval== False):
+    print("Veuillez donner l'intervalle des pages en respectant le format suivant: X-Y")
+    intrv=input().upper()
+    start=intrv[0]
+    end=intrv[2]
+    if ( len(intrv)==3 and (start <= end) and (intrv[1]=='-')):
+        bonInterval=True
+
+#Extraire la sous liste correspondante 
+startIndex,endIndex=-1,-1
+i=0
+while ((startIndex==-1) or (endIndex==-1)):
+    if (lettres[i]==start):
+        startIndex=i
+    if (lettres[i]==end):
+        endIndex=i
+    i=i+1
+        
+subLettres=lettres[startIndex:endIndex+1]
+
+file = open("subs.dic","w",encoding="utf-16")
+#file.write("\ufeff")
+
+
+# In[21]:
+
+
+page_link = 'https://www.vidal.fr/Sommaires/Substances-&.htm'
+#Modifier l'url selon la plage des valeurs voulue 
+
+
+for i in range (len(subLettres)):
+    pl=page_link.replace('&',subLettres[i])
+    page_response = requests.get(pl, timeout=5)
+    page_content = BeautifulSoup(page_response.content, "html.parser")
+    textContent = []
+    for a in page_content.findAll("ul",attrs={"class":"substances list_index has_children"}):
+        for b in a.findAll("li"):
+            b=b.text.strip("\n")+",.N+subst"+"\n"
+            file.write(b)
+file.close()
+
+
+# In[22]:
+
+
+file = open("subs.dic","r")
+
+
+# In[23]:
+
+
+print(file.read())
+
+
+# In[ ]:
+
+
+
+
