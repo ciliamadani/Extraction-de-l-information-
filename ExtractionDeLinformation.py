143f0eb9dc5fd0036e26dbd85c6859f7c7eb1a85
ciliamadani <xmoox.iclipse@hotmail.com>
2019-12-24 21:05:37 +0100

    second commit

--- ExtractionDeLinformation.py
+++ ExtractionDeLinformation.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# In[2]:
+
+
 from bs4 import BeautifulSoup
 import requests
 import re
+
+
+# In[7]:
+
 
 lettres=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 bonInterval = False
@@ -32,10 +39,15 @@ subLettres=lettres[startIndex:endIndex+1]
 file = open("subs.dic","w",encoding="utf-16")
 #file.write("\ufeff")
 
+file2 = open("infos.txt","w",encoding="utf-8")
+
+
+# In[8]:
+
 
 page_link = 'https://www.vidal.fr/Sommaires/Substances-&.htm'
 #Modifier l'url selon la plage des valeurs voulue 
-
+cpt2=0
 
 for i in range (len(subLettres)):
     pl=page_link.replace('&',subLettres[i])
@@ -43,9 +55,95 @@ for i in range (len(subLettres)):
     page_content = BeautifulSoup(page_response.content, "html.parser")
     textContent = []
     for a in page_content.findAll("ul",attrs={"class":"substances list_index has_children"}):
+        cpt=0
         for b in a.findAll("li"):
             b=b.text.strip("\n")+",.N+subst"+"\n"
+            cpt=cpt+1
             file.write(b)
+        file2.write("Le nombres des substances actives pour la letre "+subLettres[i]+" est:"+str(cpt)+"\n")
+        cpt2=cpt2+cpt
+file2.write("Le nombre total d'entites medicales par substance active est: "+str(cpt2))
 file.close()
-file = open("subs.dic","r")
-print(file.read())
+file2.close()
+
+
+# In[92]:
+
+
+corpusMedical = open("C://Users//ezi//Desktop//L3//Extraction//corpusMedical_utf8.txt",'r',encoding="utf-8")
+enriFile  =open("subst_enri.dic",'w',encoding="utf-16")
+#Preparer les expresssions regulieres
+import re     #traitement par(([A-Z]([a-z]*\S,))*|([A-Z][a-z])*)
+#traitement par (([A-Z][a-z]*),)*
+#$result=re.findall("traitement par ([A-Z][a-z]{2,15})",corpusMedical.read())
+#print(result)
+
+
+
+#Traitement de sortie :
+exp3="Traitement de sortie :\n ([A-Z]){4,15}\n"
+#esult=re.findall(exp3,corpusMedical.read())
+
+#Traitement a domicile :
+exp44="(([A-Z]{4,15}) [0-9]+ lors des douleurs|([A-Z]{4,15}) et|(([A-Z]{4,15})/S?([A-Z]{4,15})?) : [0-9]+)|([A-Z]{4,15}) ([A-Z]+).: [0-9]+-"
+
+
+result  =re.findall(exp3,corpusMedical.read())
+print(result)
+
+
+for i in range(len(result)):
+    for j in result[i]:
+        if (j!=''):
+            enriFile.write(j+'\n')
+            
+enriFile.close()
+enriFile  =open("subst_enri.dic",'r')
+
+
+# In[140]:
+
+
+corpusMedical = open("C://Users//ezi//Desktop//L3//Extraction//corpusMedical_utf8.txt",'r',encoding="utf-8")
+enriFile  =open("subst_enri.dic",'w',encoding="utf-16")
+#Preparer les expresssions regulieres
+
+import re     
+exp1 = "([a-zA-Z]{5,15}) [0-9]{1,4} mg?|([a-zA-Z]{5,15}) [0-9]{1,3} g|([a-zA-Z]{5,15}) [0-9]{1,3} gr|[a-zA-Z]{5,15} [0-9],[0-9]{1,3} ml"
+#Type de Stomie: 
+exp2="([A-Za-z]{4,15})-?([A-Za-z]*) [0-9]+,?[0-9]*MG|([A-Za-z]{4,15})-?([A-Za-z]*) [0-9]+,?[0-9]*mg|([A-Za-z]{4,15})-?([A-Za-z]*) [0-9]+,?[0-9]*mL|([A-Za-z]{4,15})-?([A-Za-z]*) [0-9]+,?[0-9]*g"
+
+exp3="Type de Stomie: ([A-Z]{4,15})"
+
+exp4 = "([a-zA-Z]{5,15} [a-zA-Z]{1,3}) [0-9]{1,3} mg|([a-zA-Z]{5,15} [a-zA-Z]{1,3}) [0-9]{1,3} g|([a-zA-Z]{5,15} [a-zA-Z]{1,3}) [0-9]{1,3} gr"
+
+exp5 = "([A-Z]{5,15}) [0-9]{1,3} [A-Z]{1,3}"
+
+exp6 = "([a-z]{5,15}) [0-9],[0-9]{1,3}\s?mL|([A-Z]{5,15}) [0-9],[0-9]{1,3} ml|([A-Z]{5,15}) [0-9],[0-9]{1,3} mL"
+
+exp7 = "(\w+[a-z]{5,15}-[a-z]{5,15}) [0-9]{1,3}g/[0-9]{1,3}mg|([A-Z]{5,15}) [0-9]\.[0-9]{1,3} mg|([A-Z][a-z]{5,15}) : [0-9]{1,3} mg"
+
+exp8 = "Vitamine ([A-Z][0-9]{0,2})"
+
+#une dose de  ..
+exp9="une dose de ([A-Z]{4,15})"
+
+#Traitement par
+exp10="prévention par ([A-Za-z]{4,15})|traitement par ([A-Za-z]{4,15})|traitement par([A-Z]{4,15}-[A-Z]{5,15})|traitement par([A-Z]{4,15} [A-Z]{4,15})|traitement par([A-Z]{4,15}) et ([A-Z]{4,15})|traitement par([A-Z]{4,15}), plutôt que par ([A-z]{4,15})"
+exp12="traitement par ([A-Z]{4,15}), ([A-Z]{4,15}) et ([A-Z]{4,15})"
+
+exp14="[A-Z]{5,15},|[A-Z]{5,15} [0-9]"
+result=re.findall(exp7,corpusMedical.read())
+print(result)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
